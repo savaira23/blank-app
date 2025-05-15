52f43bb91b93304bc0aed698fb80c6afe763b551
savaira23 <savairaaurellia@gmail.com>
2025-05-15 14:34:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Selamat datang di Website Informatika")
+st.image("4f95e729-156e-4f3d-959b-a00141376126.jpeg", width=200)
+st.write("\n")
+st.subheader("Savaira Aurellia Fatah")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+        Mari bekerja sama dalam sekelas membuat Website melalui Github dan Streamlit 
+
+    
